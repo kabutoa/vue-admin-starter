8569cc26eb3e492082ea0e5968436c6b605639aa
kabutoa <yinyun957@163.com>
2023-12-08 14:16:39 +0800

    feat: 关闭组件驼峰命名规则

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,5 +14,7 @@ module.exports = {
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    // 关闭组件驼峰命名规则
+    "vue/multi-word-component-names": "off",
   },
 };
