1f51e969d1ef36b15f3745ed8237157fc986095b
kabutoa <yinyun957@163.com>
2023-12-08 15:00:42 +0800

    feat: 按需引入element-ui

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import components from "@/utils/components";
+
+Vue.use(components);
 
 Vue.config.productionTip = false;
 
