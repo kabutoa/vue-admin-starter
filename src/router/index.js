db9d4fd3988baf852a0c8f21a3b8a5c787a1ad5b
kabutoa <yinyun957@163.com>
2023-12-08 14:16:54 +0800

    feat: init project

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import HomeView from "../views/HomeView.vue";
 
 Vue.use(VueRouter);
 
@@ -8,16 +7,17 @@ const routes = [
   {
     path: "/",
     name: "home",
-    component: HomeView,
+    // route level code-splitting
+    // this generates a separate chunk (home.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "home" */ "@/views/home/index.vue"),
   },
   {
     path: "/about",
     name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+      import(/* webpackChunkName: "about" */ "@/views/about/index.vue"),
   },
 ];
 
