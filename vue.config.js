c9f83617a097bb3b7c4c89f49ae61605c8260269
kabutoa <yinyun957@163.com>
2023-12-08 15:00:56 +0800

    feat: 配置别名

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,16 @@
 const { defineConfig } = require("@vue/cli-service");
+const path = require("path");
+
 module.exports = defineConfig({
   transpileDependencies: true,
+  configureWebpack: {
+    resolve: {
+      // 设置别名
+      alias: {
+        api: path.resolve(__dirname, "./src/api"),
+        components: path.resolve(__dirname, "./src/api"),
+        store: path.resolve(__dirname, "./src/api"),
+      },
+    },
+  },
 });
